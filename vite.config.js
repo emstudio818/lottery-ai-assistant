5d5a8f041a124199a03d768033271f675bd49581
emstudio818 <emstudio818@gmail.com>
2026-07-20 22:18:52 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,4 @@ export default defineConfig({
     server: {
         port: 3000,
     },
-    base: './',
 });
--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,4 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  base: './',
 })
